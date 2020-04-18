dee11aca60100210ca40ed2a487a4e0cb043e967
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2020-04-18 03:21:45 -0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -152,7 +152,7 @@ module.exports =
 			listenIps :
 			[
 				// change ip to your servers IP address!
-				{ ip: '23.236.49.182', announcedIp: null }
+				{ ip: '10.128.0.14', announcedIp: null }
 
 				// Can have multiple listening interfaces
 				// { ip: '::/0', announcedIp: null }
