bdb04e9acc336b4e385ecba4f5701aa378d63db8
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2020-04-18 01:18:53 -0300

    Update and rename config.example.js to config.js

--- server/config/config.js
+++ server/config/config.js
@@ -152,7 +152,7 @@ module.exports =
 			listenIps :
 			[
 				// change ip to your servers IP address!
-				{ ip: '0.0.0.0', announcedIp: null }
+				{ ip: '23.236.49.182', announcedIp: null }
 
 				// Can have multiple listening interfaces
 				// { ip: '::/0', announcedIp: null }
